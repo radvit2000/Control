be488a36e4ad1344e9693731519da8b92ba3f52d
vitaly <2004radvit@gmail.com>
2022-10-16 17:15:44 +0300

    add Iiput and output array

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string[] Input()
+{
+    Console.WriteLine("Сколько элементов должно быть?");
+    int len = Convert.ToInt32(Console.ReadLine());
+
+    string[] array = new string[len];
+    for (int i = 0; i < len; i++)
+    {
+        string str = Console.ReadLine();
+        array[i] = str;
+    }
+    return array;
+}
+
+void WriteStringArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
