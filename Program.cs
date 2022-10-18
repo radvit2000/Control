33f94d7e2a2e3aa04a10f06ca112f2c28a9f9429
vitaly <2004radvit@gmail.com>
2022-10-18 16:09:20 +0300

    add sort array

--- Program.cs
+++ Program.cs
@@ -6,16 +6,55 @@
     string[] array = new string[len];
     for (int i = 0; i < len; i++)
     {
-        string str = Console.ReadLine();
-        array[i] = str;
+        string line = Console.ReadLine();
+        array[i] = line;
     }
     return array;
 }
 
 void WriteStringArray(string[] array)
 {
-    for (int i = 0; i < array.Length; i++)
+    Console.Write($"[{array[0]}");
+    for (int i = 1; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+        Console.Write($", {array[i]}");
     }
+    Console.Write("]");
+    Console.WriteLine();
 }
+
+string[] SortArrayLessThree(string[] array)
+{
+    string[] reservArray = new string[array.Length];
+    int len = array.Length;
+    for (int i = 0; i < reservArray.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            reservArray[i] = array[i];
+        }
+        else
+        {
+            reservArray[i] = "no info";
+            len--;
+        }
+    }
+    
+    string[] sortArray = new string[len];
+    int sortId = 0;
+    for (int i = 0; i < reservArray.Length; i++)
+    {
+        if (reservArray[i] != "no info")
+        {
+            sortArray[sortId] = reservArray[i];
+            sortId++;
+        }
+    }
+    return sortArray;
+}
+
+string[] array = Input();
+string[] sortArray = SortArrayLessThree(array);
+WriteStringArray(array);
+WriteStringArray(sortArray);
+
